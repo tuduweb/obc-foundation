f2cb1e03f9742857f4608e0cc5cb4c5665262588
bin <tuduweb@qq.com>
2021-10-28 16:13:43 +0800

    拆分成子母cmake文件。

---
+++ OBCFoundation.cmake
@@ -0,0 +1,7 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(OBCFOUNDATION_SOURCES
+        ${CMAKE_CURRENT_LIST_DIR}/src/utils/OBCHelpers.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/src/utils/OBCHelpers.hpp
+        ${CMAKE_CURRENT_LIST_DIR}/src/utils/PureJson.hpp
+)
